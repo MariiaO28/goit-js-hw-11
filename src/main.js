5a499dd5108a81148fc54a9cb363539fe7a2a04a
MariiaO28 <idg.masha@gmail.com>
2024-03-03 17:06:47 +0200

    Final version with mentor's suggestions

--- src/main.js
+++ src/main.js
@@ -17,41 +17,52 @@ const loading = document.querySelector("span.loader");
 
 loading.classList.remove("loader");
 
-function submitHandling(event) {
+function handleSearchSubmit(event) {
     event.preventDefault(); 
-    gallery.innerHTML = null;
+    gallery.innerHTML = "";
     const getValue = event.currentTarget.elements.search.value.trim();
 
     if (getValue !== "") {
         loading.classList.add("loader");
-            fetchPhotos(getValue)
-                .then(data => {
-                    loading.classList.remove("loader");
-                    if (data.hits.length !== 0) {
-                        gallery.innerHTML = renderPhotos(data.hits);
-                        const lightbox = new SimpleLightbox(".gallery a", {
-                            captionsData: "alt",
-                            captionDelay: 100,
-                        });
-                        lightbox.refresh();
-                    } else {
-                        iziToast.show({
-                            iconUrl: icon,
-                            message: `Sorry, there are no images matching your search query. Please try again!`,
-                            messageColor: "#FFFFFF",
-                            color: "#FF6868",
-                            position: "topRight",
-                            progressBarColor: "#FFFFFF",
-                            close: false,
-                            timeout: 3000
-                        })
-                    }
-                })
-                .catch(error => console.log(error))
+        fetchPhotos(getValue)
+            .then(data => {
+                loading.classList.remove("loader");
+                if (data.hits.length !== 0) {
+                    gallery.innerHTML = renderPhotos(data.hits);
+                    const lightbox = new SimpleLightbox(".gallery a", {
+                        captionsData: "alt",
+                        captionDelay: 100,
+                    });
+                    lightbox.refresh();
+                } else {
+                    iziToast.show({
+                        iconUrl: icon,
+                        message: `Sorry, there are no images matching your search query. Please try again!`,
+                        messageColor: "#FFFFFF",
+                        color: "#FF6868",
+                        position: "topRight",
+                        progressBarColor: "#FFFFFF",
+                        close: false,
+                        timeout: 3000
+                    })
+                }
+            })
+            .catch(error => {
+                iziToast.show({
+                    iconUrl: icon,
+                    message: `Sorry, there was an unexpected issue running your request!`,
+                    messageColor: "#FFFFFF",
+                    color: "#FF6868",
+                    position: "topRight",
+                    progressBarColor: "#FFFFFF",
+                    close: false,
+                    timeout: 3000
+                });
+                console.error(error);
+            });
         
     };
 searchForm.reset();
 }
 
-searchForm.addEventListener("submit", submitHandling);
-
+searchForm.addEventListener("submit", handleSearchSubmit);
